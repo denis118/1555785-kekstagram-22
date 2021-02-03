f0a2521f8ca8dde735acc8e864bd948ad7bacae7
Denis Tsap <ts.dios.v@gmail.com>
2021-02-03 10:50:29 +0300

    Save

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const getNumber = function (lowerLimit, upperLimit) {
   return Math.floor(Math.random() * (upperLimit - lowerLimit + 1)) + lowerLimit;
 
   // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-}
+};
 
 getNumber();
 
@@ -29,6 +29,66 @@ const validateStringLength = function (stringToCheck, maxLength = COMMENT_MAX_LE
   }
 
   return (stringToCheck.length <= maxLength) ? true : false;
-}
+};
 
-validateStringLength();
+validateStringLength('', 0);
+
+const PHOTO_DESCRIPTION_QUANTITY = 25;
+const descriptionStorage = [];
+
+let currentPhotoId = 0;
+const setPhotoId = function () {
+  if (localStorage.getItem('currentPhotoId')) {
+    currentPhotoId = Number(localStorage.getItem('currentPhotoId'));
+    currentPhotoId += 1;
+    localStorage.setItem('currentPhotoId', currentPhotoId);
+    return currentPhotoId;
+  }
+
+  currentPhotoId += 1;
+  localStorage.setItem('currentPhotoId', currentPhotoId);
+  return currentPhotoId;
+};
+
+const explanatoryText = [
+  'Отличное лето',
+  'Хорошая зима',
+  'Плохая погода',
+  'Успешная рыбалка',
+  'Неудачный сёрфинг',
+  'Урожайный год',
+  'Новый член семьи',
+  'Встреча одноклассников',
+  'Осень пришла',
+  'Поход за грибами',
+  'Поломались на трассе',
+  'Отдых нужен всем',
+  'Марафон',
+  'Повесили кормушку для птиц',
+  'В гостях у родителей',
+  'Семь раз отмерь, один раз отрежь',
+  'Байкал',
+  'Заброшенная деревня',
+  'Наша степь',
+  'Экспериментальный пирог',
+  'Первые подстнежники',
+  'Отмечаем Хэллоуин',
+  'Последствия Хэллоуина',
+  'Исследуем окрестности',
+  'Дача',
+];
+
+const describePhoto = function () {
+  for (let i = 0; i < PHOTO_DESCRIPTION_QUANTITY; i++) {
+    let descriptionObject = {};
+    descriptionObject.id = setPhotoId();
+    descriptionObject.url = 'photos/' + descriptionObject.id + '.jpg';
+    descriptionObject.description = explanatoryText[i];
+    descriptionObject.likes = getNumber(15, 200);
+    descriptionStorage.push(descriptionObject);
+  }
+};
+
+describePhoto();
+
+localStorage.removeItem('currentPhotoId');
