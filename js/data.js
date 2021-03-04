5e14ecd99250851e08ee466648e36cb16a76e3a5
Denis Tsap <ts.dios.v@gmail.com>
2021-03-04 22:31:19 +0300

    Добавляет функцию для показа сообщения об ошибке загрузки данных

--- js/data.js
+++ js/data.js
@@ -6,6 +6,29 @@ import {
   viewImage
 } from './image-viewer.js';
 
+const onLoadingError = (error) => {
+  const errorBlock = document.querySelector('#error')
+    .content
+    .querySelector('.error');
+  const newError = errorBlock.cloneNode(true);
+  const errorTitle = newError.querySelector('.error__title');
+  const errorButton = newError.querySelector('.error__button');
+
+  errorTitle.innerText = error.name + ': ' + error.message;
+  errorButton.innerText = 'Ok';
+
+  const onErrorButtonClick = () => {
+    document.body.removeChild(newError);
+    return undefined;
+  };
+  errorButton.addEventListener('click', onErrorButtonClick, {once: true})
+
+  const errorFragment = document.createDocumentFragment();
+  errorFragment.appendChild(newError);
+  document.body.appendChild(errorFragment);
+  return undefined;
+};
+
 const getData = () => {
   fetch('https://22.javascript.pages.academy/kekstagram/data')
     .then((response) => {
@@ -22,7 +45,7 @@ const getData = () => {
       viewImage.photoDescriptions = json;
       return undefined;
     })
-    .catch((error) => alert(error));
+    .catch((error) => onLoadingError(error));
   return undefined;
 };
 
